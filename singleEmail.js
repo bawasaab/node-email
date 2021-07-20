2d78add6a9e2a542af0aa8bea93cb5783da18c55
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-07-20 11:43:24 +0530

    init

---
+++ singleEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'your-google-mail-account@gmail.com',
+		pass: 'your-google-mail-password'
+	}
+});
+
+const arrayUsersMail = ['receipient1@ymail.com', 'receipient2@gmail.com'];
+const stringUsersMail = arrayUsersMail.join(', ');
+
+var mailOptions = {
+	from: 'your-google-mail-account@gmail.com',
+	to: stringUsersMail, // myfriend@yahoo.com, myotherfriend@yahoo.com using comma seperate strings
+	subject: 'Sending Email using Node.js',
+	text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+});
