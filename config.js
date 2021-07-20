a66636908619ec90f26fe605f994f83cdac8e85d
DESKTOP-HLB995O\immi <php.freak6@gmail.com>
2021-07-20 13:46:04 +0530

    added config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = config = {
     auth: {
         service: 'gmail',
-        user: 'deepak4bawa@gmail.com',
-        pass: 'bqmsgvhcphrgpvrn'
+        user: 'your-gmail-address@gmail.com',
+        pass: 'your-gmail-password'
     },
-    from: 'deepak4bawa@gmail.com',
-    to: ['bawa_d@ymail.com'],
+    from: 'some-user-valid-gmail-address@gmail.com',
+    to: ['some-user-valid-ymail-address@ymail.com'],
     subject: 'Sending Email using Node.js cust',
     text: 'Welcome this.',
     html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
