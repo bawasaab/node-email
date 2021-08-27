32b07a415d1593c8bfddf1a7db46db1ff657a270
Deepak Bawa <bawa_d@ymail.com>
2021-08-27 11:15:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = config = {
         user: 'your-gmail-address@gmail.com',
         pass: 'your-gmail-password'
     },
-    from: 'some-user-valid-gmail-address@gmail.com',
-    to: ['some-user-valid-ymail-address@ymail.com'],
+    from: 'some-valid-user-gmail-address@gmail.com',
+    to: ['some-valid-user-yahoomail-address@yahoo.com'],
     subject: 'Sending Email using Node.js cust',
     text: 'Welcome this.',
     html: "<b>There is a new article. It's about sending emails, check it out!</b>", // html body
